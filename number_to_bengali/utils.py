4902da2e35ffc811f7145fd4ca3953759e058d61
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2020-11-08 06:47:20 +0600

    feat: added segment generation
    segments in this context are the nubmer devided into hundreds, thousands
    and so on.

--- number_to_bengali/utils.py
+++ number_to_bengali/utils.py
@@ -1,3 +1,6 @@
+import math
+
+
 def input_sanitizer(number):
     if isinstance(number, float) or isinstance(number, int) or isinstance(number, str):
         isfloat = isinstance(number, float)
@@ -13,3 +16,21 @@ def input_sanitizer(number):
         return isfloat, number
     else:
         return None, None
+
+
+def generate_segments(number):
+    """
+    Generating the unit segments such as koti, lokkho
+    """
+    segments = dict()
+    segments['koti'] = math.floor(number/10000000)
+    number = number % 10000000
+    segments['lokkho'] = math.floor(number/100000)
+    number = number % 100000
+    segments['hazar'] = math.floor(number/1000)
+    number = number % 1000
+    segments['sotok'] = math.floor(number/100)
+    number = number % 100
+    segments['ekok'] = number
+
+    return segments
