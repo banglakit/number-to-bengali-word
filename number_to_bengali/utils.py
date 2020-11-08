b4ea2ac062e62ffd33d6c66b60de3fe73eefda99
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2020-11-08 06:16:30 +0600

    feat: checking for input datatype
    1. chekcing if the input is anything other than int, float or string
    2. returning None if the string to int/float convertion fails

--- number_to_bengali/utils.py
+++ number_to_bengali/utils.py
@@ -1,9 +1,15 @@
 def input_sanitizer(number):
-    isfloat = isinstance(number, float)
-    if isinstance(number, str):
-        isfloat = "." in number
-        if isfloat:
-            number = float(number)
-        else:
-            number = int(number)
-    return isfloat, number
+    if isinstance(number, float) or isinstance(number, int) or isinstance(number, str):
+        isfloat = isinstance(number, float)
+        if isinstance(number, str):
+            try:
+                isfloat = "." in number
+                if isfloat:
+                    number = float(number)
+                else:
+                    number = int(number)
+            except ValueError:
+                return None, None
+        return isfloat, number
+    else:
+        return None, None
